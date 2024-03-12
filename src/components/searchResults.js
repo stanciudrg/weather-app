9da95dd07adcd7449a3eb92d03b06bf3b4658bea
stanciudrg <stanciudragos1337@gmail.com>
2024-03-12 12:12:40 +0200

    Create searchResults.js module and fetchSearchResults function
    The function fetches the search JSON file for a specified parameter
    using the customFetch function

---
+++ src/components/searchResults.js
@@ -0,0 +1,10 @@
+import customFetch from "./customFetch";
+
+// Fetches the search JSON file for a specified parameter
+// using the customFetch function
+const fetchSearchResults = async (parameters) => {
+    const url = `https://api.weatherapi.com/v1/search.json?key=bd957ce5f33f49e692b105538240603&q=${parameters}`;
+    const searchResults = await customFetch(url);
+    return searchResults;
+  };
+  
