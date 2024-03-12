b052bd97ea1cb0298643fac9028715bad802d47b
stanciudrg <stanciudragos1337@gmail.com>
2024-03-12 12:46:03 +0200

    Create getSearchResults function in searchResults.js module
    The function fetches the searchResults array and returns a new array of
    objects that contain only the data needed for this app

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -2,8 +2,27 @@ import customFetch from "./customFetch";
 
 // Fetches the search JSON file for a specified parameter
 // using the customFetch function
-const fetchSearchResults = async (parameters) => {
-    const url = `https://api.weatherapi.com/v1/search.json?key=bd957ce5f33f49e692b105538240603&q=${parameters}`;
-    const searchResults = await customFetch(url);
-    return searchResults;
-  };
+export const fetchSearchResults = async (parameters) => {
+  const url = `https://api.weatherapi.com/v1/search.json?key=bd957ce5f33f49e692b105538240603&q=${parameters}`;
+  const searchResults = await customFetch(url);
+  return searchResults;
+};
+
+// Fetches the searchResults array and returns a new array of objects that contain only the data
+// needed for this app
+export const getSearchResults = async (parameters) => {
+  const searchResults = await fetchSearchResults(parameters);
+  const simplifiedSearchResults = [];
+
+  searchResults.forEach((result) => {
+    const customSearchResult = {
+      id: result.id,
+      name: result.name,
+      country: result.country,
+    };
+
+    simplifiedSearchResults.push(customSearchResult);
+  });
+
+  return simplifiedSearchResults;
+};
