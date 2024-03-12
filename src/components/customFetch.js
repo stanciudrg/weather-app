6d0de4ad0a0a9eaf4e0559816cecf3aee0811947
stanciudrg <stanciudragos1337@gmail.com>
2024-03-12 11:23:25 +0200

    Create customFetch module
    The module fetches the provided url and returns the JSON version of
    the response

---
+++ src/components/customFetch.js
@@ -0,0 +1,10 @@
+
+// Fetches the url and returns the JSON version of the response
+export default async function customFetch(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    const data = await response.json();
+    return data;
+}
