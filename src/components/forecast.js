ca628a009ae95dddde85181c955b491c11ef66d1
stanciudrg <stanciudragos1337@gmail.com>
2024-03-13 13:58:58 +0200

    Transform simplifyFetchedData into a static class method

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,33 +1,4 @@
 import customFetch from "./customFetch";
-
-// Returns an object containing the needed target properties only
-const simplifyFetchedData = (target) => {
-  const simplifiedForecastData = {
-    days: [],
-  };
-  target.forecast.forecastday.forEach((day) => {
-    const customDay = {
-      date: day.date,
-      condition: day.day.condition,
-      avgTemp: {
-        c: day.day.avgtemp_c,
-        f: day.day.avgtemp_f,
-      },
-      maxTemp: {
-        c: day.day.maxtemp_c,
-        f: day.day.maxtemp_f,
-      },
-      minTemp: {
-        c: day.day.mintemp_c,
-        f: day.day.mintemp_f,
-      },
-      chanceOfRain: day.day.daily_chance_of_rain,
-    };
-    simplifiedForecastData.days.push(customDay);
-  });
-
-  return simplifiedForecastData;
-};
 export default class Forecast {
   constructor(location) {
     this.location = location;
@@ -38,8 +9,8 @@ export default class Forecast {
 
   async init() {
     try {
-      const forecastData = await this.fetchData();;
-      const simplifiedFetchedData = simplifyFetchedData(forecastData);
+      const forecastData = await this.fetchData();
+      const simplifiedFetchedData = Forecast.simplifyFetchedData(forecastData);
       this.setData(simplifiedFetchedData);
       this.displayData();
     } catch (error) {
@@ -77,8 +48,37 @@ export default class Forecast {
   async fetchData() {
     const url = `https://api.weatherapi.com/v1/forecast.json?key=bd957ce5f33f49e692b105538240603&q=${this.location}&days=3`;
     const forecastData = await customFetch(url);
-  
+
     return forecastData;
+  }
+
+  // Returns an object containing the needed target properties only
+  static simplifyFetchedData(target) {
+    const simplifiedForecastData = {
+      days: [],
+    };
+    target.forecast.forecastday.forEach((day) => {
+      const customDay = {
+        date: day.date,
+        condition: day.day.condition,
+        avgTemp: {
+          c: day.day.avgtemp_c,
+          f: day.day.avgtemp_f,
+        },
+        maxTemp: {
+          c: day.day.maxtemp_c,
+          f: day.day.maxtemp_f,
+        },
+        minTemp: {
+          c: day.day.mintemp_c,
+          f: day.day.mintemp_f,
+        },
+        chanceOfRain: day.day.daily_chance_of_rain,
+      };
+      simplifiedForecastData.days.push(customDay);
+    });
+
+    return simplifiedForecastData;
   }
 
   insertInto(element) {
