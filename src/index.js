427c979106266ce8ff2f1f61d517fed653a8ed06
stanciudrg <stanciudragos1337@gmail.com>
2024-03-14 11:10:03 +0200

    Add normalize.css file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './normalize.css';
+import './style.css';
+import CurrentWeather from "./components/CurrentWeather";
+import Forecast from "./components/Forecast";
+import SearchResults from "./components/SearchResults";
